e19bd3aca3562508979d1c7ba46c779a124755f5
JunseoMin <minjs4562@gmail.com>
2023-09-17 17:41:11 +0900

    hw fixed

--- src/homeworkone.m
+++ src/homeworkone.m
@@ -1,42 +1,101 @@
-%% simulink first work_space
+%% simulink work_space
 clear all
 clc
 
-%% Basical
-% result=sim('untitled');% sim('slx file name')
-% 
-% %plot
-% x=result.simout.time; % check to workspace block
-% y=result.simout.data;
-% 
-% plot(x,y)
-
-
-%% HW
+%% HW(torque)
 % block diagram in p.7
+Va = 48;       % V
 
+La = 0.000658;     % H (dataseet * 1/1000)
+Ra = 1.76;  % Ohm
 
-% select veriable careful!
-Va=1;
+Kt=0.0683;       % Nm/A (dataseet * 1/1000)
+Ke=0.0683;       % Nm/A (dataseet * 1/1000)
 
-La = 1;
-Ra = 0.01;
+Tl=[0,0.5,1];       % Nm 
 
-Kt=1;
-Ke=1;
+J=0.00000995;      % kgm^2 (dataseet * 1/10000000)
+b=0.00000995/0.00376;      % Mechanical Tie Constant (J/t Nms/rad)
 
-Tl=0;
+angle_res = cell(length(Tl), 1);
+anglevel_res = cell(length(Tl),1);
+% plot
+for i = 1: length(Tl)
+    Tl_=Tl(i);
 
-J=0.2;
-b=0.1;
-%% plot
+    res=sim('moterhomework');
+    
+    time_=res.torque.time;
+    angle_vel=res.torque.data;
 
-%res=sim('motor_modeling');
-%x1=res.current.time;
-%y1=res.current.data;
+    angle = cumtrapz(time_,angle_vel)*57.2958;
+    
+    anglevel_res{i} = {time_,angle_vel};
+    angle_res{i} = {time_,angle};
+end
 
-%x2=res.torque.time;
-%y2=res.torque.data;
+figure;
+hold on;
+xlabel('time (s)');
+ylabel('rad/s');
 
-%plot(x1,y1)
-%plot(x2,y2)
+%for i = 1:length(Tl)
+%    plot(angle_res{i}{1},angle_res{i}{2})
+%end
+
+for i = 1:length(Tl)
+    plot(anglevel_res{i}{1},anglevel_res{i}{2})
+end
+
+title('Angular Velocity');
+legend('Load Torque = 0','Load Torque = 0.5','Load Torque = 1','Location', 'best');
+grid on;
+
+%% HW(input volt)
+% block diagram in p.7
+Va = [5,12,24,48];       % V
+
+La = 0.000658;     % H (dataseet * 1/1000)
+Ra = 1.76;  % Ohm
+
+Kt=0.0683;       % Nm/A (dataseet * 1/1000)
+Ke=0.0683;       % Nm/A (dataseet * 1/1000)
+
+Tl=0;       % Nm 
+
+J=0.00000995;      % kgm^2 (dataseet * 1/10000000)
+b=0.00000995/0.00376;      % Mechanical Tie Constant (J/t Nms/rad)
+
+angle_res = cell(length(Va), 1);
+anglevel_res = cell(length(Va),1);
+% plot
+for i = 1: length(Va)
+    Va_=Va(i);
+
+    res=sim('moterhomework');
+    
+    time_=res.torque.time;
+    angle_vel=res.torque.data;
+
+    angle = cumtrapz(time_,angle_vel)*57.2958;
+    
+    anglevel_res{i} = {time_,angle_vel};
+    angle_res{i} = {time_,angle};
+end
+
+figure;
+hold on;
+xlabel('time (s)');
+ylabel('rad/s');
+
+%for i = 1:length(Va)
+%    plot(angle_res{i}{1},angle_res{i}{2})
+%end
+
+for i = 1:length(Va)
+    plot(anglevel_res{i}{1},anglevel_res{i}{2})
+end
+
+title('Angular Velocity');
+legend('Load Torque = 0','Load Torque = 0.5','Load Torque = 1','Location', 'best');
+grid on;
